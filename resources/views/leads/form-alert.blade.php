f316f627876d135f5e8fdf864018b6120401ba46
jonathan <jlee@ingenlogic.com>
2017-09-13 09:34:35 -0700

    final alert

--- resources/views/leads/form-alert.blade.php
+++ resources/views/leads/form-alert.blade.php
@@ -17,18 +17,20 @@
         <tr><th><input type='checkbox' class='alert-checkall' id='checkall'></th> <th>Name</th><th>Position</th> <th>Title</th> <th>Phone</th> <th>Email</th> </tr>
       </thead>
       <tbody>
-      @forelse ($followers as $follower)
-        <tr class="btn-row-add alert-user-row">
-          <td>{!! Form::checkbox('is_read[]', enc_id($follower->id), ($follower->is_read == 0) ? 1 : 0, ['class' => 'alert-read']) !!}</td>
-          <td class="row-name">{{ trim($follower->fname.' '.$follower->lname) }}</td>
-          <td class="row-position">{{ $follower->access_lv }}</td>
-          <td class="row-title">{{ $follower->title }}</td>
-          <td class="row-phone">{{ $follower->tel }}</td>
-          <td class="row-email">{{ $follower->email }}</td>
-        </tr>
-      @empty
-        <tr><td colspan="9" class="not-found">* 0 Followers found.</td></tr>
-      @endforelse
+        @if(count($followers) > 0)
+          @foreach ($followers as $follower)
+            <tr class="btn-row-add alert-user-row">
+              <td>{!! Form::checkbox('is_read[]', enc_id($follower->id), ($follower->is_read == 0) ? 1 : 0, ['class' => 'alert-read']) !!}</td>
+              <td class="row-name">{{ trim($follower->fname.' '.$follower->lname) }}</td>
+              <td class="row-position">{{ $follower->access_lv }}</td>
+              <td class="row-title">{{ $follower->title }}</td>
+              <td class="row-phone">{{ $follower->tel }}</td>
+              <td class="row-email">{{ $follower->email }}</td>
+            </tr>
+          @endforeach
+        @else
+          <tr><td colspan="9" class="not-found">* 0 Followers found.</td></tr>
+        @endif
       </tbody>
     </table>
   </div>
